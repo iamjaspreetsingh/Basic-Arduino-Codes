d28342941e2b653d377a0b25e55b9fb9d0f5b2c4
iamjaspreetsingh <jsk1961998@gmail.com>
2018-01-12 19:43:57 +0530

    ultrasonic

--- Button/button.ino
+++ Button/button.ino
@@ -1,17 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(57600);
-pinMode(10, OUTPUT);
-pinMode(3, INPUT_PULLUP);
+pinMode(4, OUTPUT);
+pinMode(2, INPUT_PULLUP);
 
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- if(digitalRead(3))
-  digitalWrite(10,HIGH);
+ if(digitalRead(2))
+  digitalWrite(4,LOW);
   else
-  digitalWrite(10,LOW);
+  digitalWrite(4,HIGH);
 // floating state of Serial.println(analogRead(A1));
 }
--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,19 +1,55 @@
+int pin[]={5,6,7,8,9};
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(57600);
 pinMode(3, OUTPUT);
 pinMode(11, INPUT);
-
-  
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
 }
 
 void loop() {
+ 
   // put your main code here, to run repeatedly:
-  digitalWrite(3,LOW);
-delayMicroseconds(2);
-  digitalWrite(3,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(3,LOW);
-Serial.println(340*pulseIn(11,HIGH));
+ digitalWrite(3,LOW);
+ delayMicroseconds(2);
+ digitalWrite(3,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(3,LOW);
+  
+int x=pulseIn(11,HIGH);
+Serial.println(x);
+if(x>0&&x<200)
+{for(int i=0;i<5;i++)
+digitalWrite(pin[i],HIGH);
+}
+else if((x>=200)&&(x<400))
+{for(int i=0;i<4;i++)
+digitalWrite(pin[i],HIGH);
+for(int j=4;j<5;j++)
+digitalWrite(pin[j],LOW);}
 
+else if((x>=400)&&(x<600))
+{for(int i=0;i<3;i++)
+digitalWrite(pin[i],HIGH);
+for(int j=3;j<5;j++)
+digitalWrite(pin[j],LOW);}
+
+else if((x>=600)&&(x<800))
+{for(int i=0;i<2;i++)
+digitalWrite(pin[i],HIGH);
+for(int j=2;j<5;j++)
+digitalWrite(pin[j],LOW);}
+
+else if((x>=800)&&(x<1000))
+{digitalWrite(pin[0],HIGH);
+for(int j=1;j<5;j++)
+digitalWrite(pin[j],LOW);
+}
+else 
+for(int i=0;i<5;i++)
+digitalWrite(pin[i],LOW);
 }
